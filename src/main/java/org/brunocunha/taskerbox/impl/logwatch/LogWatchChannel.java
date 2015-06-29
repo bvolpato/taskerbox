a38b00475237b41c40a430408d0dbb33b8048cae
Bruno Candido Volpato da Cunha <brunocvcunha@gmail.com>
2015-06-28 22:01:50 -0300

    reducing bandwidth usage in log watcher
    reducing bandwidth usage in log watcher. now it does a HEAD request to
    see if the length of the log changed

--- src/main/java/org/brunocunha/taskerbox/impl/logwatch/LogWatchChannel.java
+++ src/main/java/org/brunocunha/taskerbox/impl/logwatch/LogWatchChannel.java
@@ -9,7 +9,6 @@ import lombok.extern.log4j.Log4j;
 import org.brunocunha.taskerbox.core.TaskerboxChannel;
 import org.brunocunha.taskerbox.core.annotation.TaskerboxField;
 import org.brunocunha.taskerbox.core.http.TaskerboxHttpBox;
-import org.hibernate.validator.constraints.URL;
 
 /**
  * Log Watch Channel
@@ -25,20 +24,55 @@ public class LogWatchChannel extends TaskerboxChannel<String> {
 	@Getter @Setter
 	private String[] url;
 
-	@TaskerboxField("Arquivo Resultados")
+	@TaskerboxField("Results File")
 	@Getter @Setter
 	private File saveResultFile;
+	
+	@TaskerboxField("Control by Size")
+	@Getter @Setter
+	private boolean controlBySize = true;
+	
+	private long[] size;
+	
+	@Override
+	public void setup() {
+		size = new long[url.length];
+	}
 	
 	@Override
 	protected void execute() {
 
 		try {
-			for (String uniqueUrl : url) {
-				logDebug(log, "Checking [" + uniqueUrl + "]");
-				String responseBody = TaskerboxHttpBox.getInstance().getStringBodyForURL(uniqueUrl);
-				logDebug(log, "Got LogWatch Response: " + responseBody.length() + " bytes");
-				perform(responseBody);
+			url:
+			for (int x = 0; x < url.length; x++) {
+				String uniqueUrl = url[x];
+				
+				try {
+					logDebug(log, "Checking [" + uniqueUrl + "]");
+					
+					if (controlBySize) {
+						long uniqueSize = TaskerboxHttpBox.getInstance().getResponseSizeForURL(uniqueUrl);
+						
+						logDebug(log, "Size of response: " + uniqueUrl + " - " + uniqueSize);
+						
+						if (size[x] == uniqueSize) {
+							logDebug(log, "Same size! Skipping URL " + uniqueUrl);
+							continue url;
+						}
+						
+						size[x] = uniqueSize;
+					}
+					
+					String responseBody = TaskerboxHttpBox.getInstance().getStringBodyForURL(uniqueUrl);
+					
+					logDebug(log, "Got LogWatch Response: " + uniqueUrl + " - " + responseBody.length() + " bytes");
+					
+					perform(responseBody);
+				} catch (Exception e) {
+					logError(log, "Error watching log for " + getId() + " - " + uniqueUrl, e);
+				}
 			}
+		
 		} catch (Exception e) {
 			logError(log, "Error watching log for " + getId(), e);
 		}
