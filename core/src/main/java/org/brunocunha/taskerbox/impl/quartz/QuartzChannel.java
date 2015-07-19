6a63a5250fcf7223f92410f27b413cc0af3161e9
Bruno Candido Volpato da Cunha <brunocvcunha@gmail.com>
2015-07-19 14:51:53 -0300

    adding quartz for more generic scheduling

---
+++ core/src/main/java/org/brunocunha/taskerbox/impl/quartz/QuartzChannel.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (C) 2015 Bruno Candido Volpato da Cunha (brunocvcunha@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.brunocunha.taskerbox.impl.quartz;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+import org.brunocunha.taskerbox.core.TaskerboxChannel;
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerFactory;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+@Log4j
+public class QuartzChannel extends TaskerboxChannel<Long> {
+
+  @Getter
+  @Setter
+  private List<String> triggers;
+
+  @Getter
+  @Setter
+  private boolean daemon = true;
+
+  @Override
+  protected void execute() throws Exception {
+    SchedulerFactory schedFact = new StdSchedulerFactory();
+    Scheduler sched = schedFact.getScheduler();
+    sched.start();
+
+
+    JobDataMap map = new JobDataMap();
+    map.put("channel", this);
+
+    JobDetail job =
+        JobBuilder.newJob(QuartzChannelJob.class).withIdentity("CronJob", getId())
+            .setJobData(map).storeDurably(true).build();
+    
+    sched.addJob(job, true);
+    
+    for (String triggerStr : triggers) {
+      Trigger trigger =
+          TriggerBuilder.newTrigger().withIdentity("CronTrigger"+triggerStr, getId())
+          .forJob(job)
+          .withSchedule(CronScheduleBuilder.cronSchedule(triggerStr)).build();
+      
+      sched.scheduleJob(trigger);
+      
+    }
+
+
+  }
+
+  @Override
+  protected String getItemFingerprint(Long entry) {
+    return entry.toString();
+  }
+
+}
+
---
+++ core/src/main/java/org/brunocunha/taskerbox/impl/quartz/QuartzChannelJob.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (C) 2015 Bruno Candido Volpato da Cunha (brunocvcunha@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.brunocunha.taskerbox.impl.quartz;
+
+import lombok.extern.log4j.Log4j;
+
+import org.brunocunha.taskerbox.core.TaskerboxChannel;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+@Log4j
+public class QuartzChannelJob implements Job {
+  @Override
+  public void execute(JobExecutionContext context) throws JobExecutionException {
+    log.info("Channel Job Invoked");
+    
+    TaskerboxChannel<Long> originChannel = (TaskerboxChannel<Long>) context.getJobDetail().getJobDataMap().get("channel");
+    originChannel.perform(System.nanoTime());
+  }
+}
