5117b7d7cf6ddbbf9a7b0c72a88dc4970e486338
Bruno Candido Volpato da Cunha <brunocvcunha@gmail.com>
2015-11-24 00:16:11 -0200

    seeker feature to search based on zip

--- core/src/main/java/org/brunocvcunha/taskerbox/impl/jobs/LinkedInJobSeeker.java
+++ core/src/main/java/org/brunocvcunha/taskerbox/impl/jobs/LinkedInJobSeeker.java
@@ -66,6 +66,11 @@ public class LinkedInJobSeeker extends DefaultJobSearchChannel {
 
   @Getter
   @Setter
+  @TaskerboxField("Postal Code")
+  private String postalCode;
+  
+  @Getter
+  @Setter
   private File toApplyFile;
 
   @Getter
@@ -319,6 +324,10 @@ public class LinkedInJobSeeker extends DefaultJobSearchChannel {
                   + "&countryCode=" + country
                   + "&sortBy=DD&orig=JSHP&distance=100&locationType=I&openFacets=L,C,N&page_num="
                   + x + "&pt=jobs&f_TP=" + dateFacet;
+          if (postalCode != null && !postalCode.isEmpty()) {
+            seekUrl += "&postalCode=" + postalCode;
+          }
+          
           logInfo(log, "... Seeking " + seekUrl);
           HttpEntity entity = TaskerboxHttpBox.getInstance().getEntityForURL(seekUrl);
           String result = TaskerboxHttpBox.getInstance().readResponseFromEntity(entity);
